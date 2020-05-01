8d13c6da52cb348514162216060244d868cb6a5c
elganiesta <54721994+elganiesta@users.noreply.github.com>
2020-05-01 01:31:02 +0000

    8 ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue[700],
+          appBar: AppBar(
+            backgroundColor: Colors.blue[900],
+            title: Text('Ask Me Any Thing'),
+          ),
+          body: Ball(),
+        ),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ball_number = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+            ball_number = Random().nextInt(5) +1 ;
+          });
+          },
+          child: Image.asset('images/ball$ball_number.png'),
+        ),
+      ),
+    );
+  }
+}
